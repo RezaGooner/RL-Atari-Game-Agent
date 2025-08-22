22b1243819eb50bc64d32a5285fd86a2e577ab04
𝐑𝐞𝐳𝐚 𝐀𝐬𝐚𝐝𝐢 <149728623+RezaGooner@users.noreply.github.com>
2025-08-22 14:37:17 +0330

    Update train.py

--- pong/train.py
+++ pong/train.py
@@ -4,13 +4,13 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 
-# ساخت محیط Atari Pong
 env = make_atari_env('ALE/Pong-v5', n_envs=1, seed=42)  
 env = VecFrameStack(env, n_stack=4)
 
-# مدل PPO
 model = PPO('CnnPolicy', env, verbose=1, tensorboard_log="./ppo_pong_tensorboard/")
 model.learn(total_timesteps=1_000_000)
 
 model.save("ppo_pong_agent")
 env.close()
+
+# Github.com/RezaGooner
